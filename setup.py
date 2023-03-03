eb79514334bf414f25588543add9c4625ffa78b0
h1019753743 <1019753743@qq.com>
2023-03-03 18:27:09 +0800

    fix no data issue

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     include_package_data=True,
     keywords='addana',
     name='addana',
-    packages=find_packages(include=['addana', 'addana.*','addana.data']),
+    packages=find_packages( ),
     # package_dir={'addana': 'addana'},
     package_data={
         'addana': ['*.*'],
